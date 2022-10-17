8746668f6c09d2f4cfc4ff7474f2a61b3bc5175d
hylasxl <vietpham062@gmail.com>
2022-10-17 19:56:12 +0700

    git cache cleared

---
+++ VTC_NETWORKING_124010122002_PHAMTRUNGVIET_SERVER/Server/Server/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnsend = new System.Windows.Forms.Button();
+            this.message = new System.Windows.Forms.TextBox();
+            this.listview = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // btnsend
+            // 
+            this.btnsend.Location = new System.Drawing.Point(806, 445);
+            this.btnsend.Name = "btnsend";
+            this.btnsend.Size = new System.Drawing.Size(86, 47);
+            this.btnsend.TabIndex = 5;
+            this.btnsend.Text = "Send";
+            this.btnsend.UseVisualStyleBackColor = true;
+            this.btnsend.Click += new System.EventHandler(this.btnsend_Click);
+            // 
+            // message
+            // 
+            this.message.Location = new System.Drawing.Point(8, 445);
+            this.message.Multiline = true;
+            this.message.Name = "message";
+            this.message.Size = new System.Drawing.Size(792, 47);
+            this.message.TabIndex = 4;
+            // 
+            // listview
+            // 
+            this.listview.Location = new System.Drawing.Point(8, 22);
+            this.listview.Name = "listview";
+            this.listview.Size = new System.Drawing.Size(884, 417);
+            this.listview.TabIndex = 3;
+            this.listview.UseCompatibleStateImageBehavior = false;
+            this.listview.View = System.Windows.Forms.View.List;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnsend;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(904, 508);
+            this.Controls.Add(this.btnsend);
+            this.Controls.Add(this.message);
+            this.Controls.Add(this.listview);
+            this.Name = "Form1";
+            this.Text = "Server";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btnsend;
+        private TextBox message;
+        private ListView listview;
+    }
+}
